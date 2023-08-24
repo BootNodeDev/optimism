e454f44bad4da58b00921f5524ee9daae063c26d
Paterson <86971279+paterson1@users.noreply.github.com>
2023-08-24 18:11:17 -0300

    add tests

--- packages/contracts-bedrock/contracts/test/L2ERC1155Bridge.t.sol
+++ packages/contracts-bedrock/contracts/test/L2ERC1155Bridge.t.sol
@@ -153,4 +153,112 @@ contract L2ERC1155Bridge_Test is Messenger_Initializer {
         // Token is burned. Amount was started with 5
         assertEq(localToken.balanceOf(alice, tokenId), 4);
     }
+
+    /// @dev Tests that `bridgeERC1155` reverts if the local token is the zero address.
+    function test_bridgeERC1155_localTokenZeroAddress_reverts() external {
+        // Bridge the token by an amount of 2.
+        vm.prank(alice);
+        vm.expectRevert();
+        bridge.bridgeERC1155(address(0), address(remoteToken), tokenId, 2, 1234, hex"5678");
+
+        // Token is not locked in the bridge. The amount is the original one
+        assertEq(localToken.balanceOf(alice, tokenId), 5);
+    }
+
+    /// @dev Tests that `bridgeERC1155` reverts if the remote token is the zero address.
+    function test_bridgeERC1155_remoteTokenZeroAddress_reverts() external {
+        // Bridge the token.
+        vm.prank(alice);
+        vm.expectRevert("L2ERC1155Bridge: remote token cannot be address(0)");
+        bridge.bridgeERC1155(address(localToken), address(0), tokenId, 2, 1234, hex"5678");
+
+        // Token is not locked in the bridge.
+        assertEq(localToken.balanceOf(alice, tokenId), 5);
+    }
+
+    /// @dev Tests that `bridgeERC1155` reverts if the caller is not the token owner.
+    function test_bridgeERC1155_wrongOwner_reverts() external {
+        // Bridge the token.
+        vm.prank(bob);
+        vm.expectRevert("ERC1155: burn amount exceeds balance");
+        bridge.bridgeERC1155(address(localToken), address(remoteToken), tokenId, 2, 1234, hex"5678");
+
+        // Token is not locked in the bridge.
+        assertEq(localToken.balanceOf(alice, tokenId), 5);
+    }
+
+    /// @dev Tests that `bridgeERC1155To` correctly bridges a token
+    ///      and burns it on the origin chain.
+    function test_bridgeERC1155To_succeeds() external {
+        // Expect a call to the messenger.
+        vm.expectCall(
+            address(L2Messenger),
+            abi.encodeCall(
+                L2Messenger.sendMessage,
+                (
+                    address(otherBridge),
+                    abi.encodeCall(
+                        L1ERC1155Bridge.finalizeBridgeERC1155,
+                        (address(remoteToken), address(localToken), alice, bob, tokenId, 2, hex"5678")
+                    ),
+                    1234
+                )
+            )
+        );
+
+        // Expect an event to be emitted.
+        vm.expectEmit(true, true, true, true);
+        emit ERC1155BridgeInitiated(
+            address(localToken),
+            address(remoteToken),
+            alice,
+            bob,
+            tokenId,
+            2,
+            hex"5678"
+        );
+
+        // Bridge the token.
+        vm.prank(alice);
+        bridge.bridgeERC1155To(
+            address(localToken),
+            address(remoteToken),
+            bob,
+            tokenId,
+            2,
+            1234,
+            hex"5678"
+        );
+
+        // Token is burned.
+        assertEq(localToken.balanceOf(alice, tokenId), 3);
+    }
+
+}
+
+/// @dev A non-compliant ERC721 token that does not implement the full ERC721 interface.
+///      This is used to test that the bridge will revert if the token does not claim to
+///      support the ERC721 interface.
+contract NonCompliantERC721 {
+    address internal immutable owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function ownerOf(uint256) external view returns (address) {
+        return owner;
+    }
+
+    function remoteToken() external pure returns (address) {
+        return address(0x01);
+    }
+
+    function burn(address, uint256) external {
+        // Do nothing.
+    }
+
+    function supportsInterface(bytes4) external pure returns (bool) {
+        return false;
+    }
 }
